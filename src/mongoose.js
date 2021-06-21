5937abd96610aa386bbb9f9fa48630f41a9043f7
Auro <auro@smarttersstudio.com>
2021-06-21 10:17:47 +0530

    init project

---
+++ src/mongoose.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import logger from "./logger";
+
+export default function (app) {
+  mongoose
+    .connect(app.get("mongodb"), {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => {
+      logger.error(err);
+      process.exit(1);
+    });
+
+  app.set("mongooseClient", mongoose);
+}
